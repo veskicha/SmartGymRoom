e7c0d3b0ae93dfd63d5d2717e1600b65cc44a633
greendm1202 <v.shterev@student.utwente.nl>
2023-10-17 14:12:03 +0200

    added animation of starting screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,12 @@ android {
 
 dependencies {
 
+    implementation ("io.reactivex.rxjava2:rxandroid:2.0.1")
+    implementation ("io.reactivex.rxjava2:rxjava:2.0.8")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.databinding:baseLibrary:3.2.0-alpha11")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
