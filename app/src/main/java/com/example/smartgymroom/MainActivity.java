4860ee3ebd409da0dc7bc174d273daaf641c43c5
greendm1202 <v.shterev@student.utwente.nl>
2023-10-17 11:18:01 +0200

    bletooth search

--- app/src/main/java/com/example/smartgymroom/MainActivity.java
+++ app/src/main/java/com/example/smartgymroom/MainActivity.java
@@ -1,23 +1,40 @@
 package com.example.smartgymroom;
 
-import static android.content.ContentValues.TAG;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean init = false;
+    private MediaManager mediaManager;
+    private static final String TAG = "DebugLogs";
+    private BluetoothLeScanner scanner;
+    private final List<ScanFilter> filters = new ArrayList<>();
+    private final ScanSettings scanSettings = new ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+            .build();
 
     @SuppressLint("MissingPermission")
     @Override
@@ -25,61 +42,101 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        mediaManager = new MediaManager(this);
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        scanner = adapter.getBluetoothLeScanner();
 
-            @SuppressLint("MissingPermission")
-            @Override
-            public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-                if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    Log.i(TAG, "Connected to GATT server.");
-                    Log.i(TAG, "Attempting to start service discovery:" + gatt.discoverServices());
+        if (checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+        if (scanner != null && !init) {
+            scanner.startScan(filters, scanSettings, scanCallback);
+            Log.d(TAG, "scan started");
+        } else {
+            Log.e(TAG, "could not get scanner object");
+        }
 
-                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                    Log.i(TAG, "Disconnected from GATT server.");
-                }
+    }
+
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            if (init) {
+                scanner.stopScan(this);
             }
+            @SuppressLint("MissingPermission") String deviceName = device.getName();
+            if ("ESP32".equals(deviceName)) {
+                Log.d(TAG, "ESP32 device found, attempting to connect...");
+                scanner.stopScan(this);
+                init = true;
 
-            @SuppressLint("MissingPermission")
-            @Override
-            public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-                if (status == BluetoothGatt.GATT_SUCCESS) {
-                    // TODO: Here you can loop through available services and characteristics
-                    UUID SERVICE_UUID = UUID.fromString("2fe4da9b-57da-43a8-b8f9-8877344d7dc5");
-                    UUID CHARACTERISTIC_UUID = UUID.fromString("541426fd-debd-471d-8e1c-a4a18a837028");
-                    BluetoothGattService service = gatt.getService(SERVICE_UUID);
-                    if (service != null) {
-                        BluetoothGattCharacteristic characteristic = service.getCharacteristic(CHARACTERISTIC_UUID);
-                        gatt.setCharacteristicNotification(characteristic, true);
-                        BluetoothGattDescriptor descriptor = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
-                        descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                        gatt.writeDescriptor(descriptor);
-                        if ((characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_READ) != 0) {
-                            gatt.readCharacteristic(characteristic);
-                            Log.d(TAG, "Reading characteristics..");
-                        }
+                // TODO: Add your connection logic here. You'll probably need a GATT callback.
+                device.connectGatt(MainActivity.this, false, gattCallback);
 
+            } else {
+                Log.d(TAG, "Device found but not ESP32, continuing scan...");
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Log.e(TAG, "Scan failed with error: " + errorCode);
+            if (!init) {
+                scanner.startScan(filters, scanSettings, this);
+            }
+        }
+
+
+    };
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery:" + gatt.discoverServices());
+
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "Disconnected from GATT server.");
+            }
+        }
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                // TODO: Here you can loop through available services and characteristics
+                UUID SERVICE_UUID = UUID.fromString("2fe4da9b-57da-43a8-b8f9-8877344d7dc5");
+                UUID CHARACTERISTIC_UUID = UUID.fromString("541426fd-debd-471d-8e1c-a4a18a837028");
+                BluetoothGattService service = gatt.getService(SERVICE_UUID);
+                if (service != null) {
+                    BluetoothGattCharacteristic characteristic = service.getCharacteristic(CHARACTERISTIC_UUID);
+                    gatt.setCharacteristicNotification(characteristic, true);
+                    BluetoothGattDescriptor descriptor = characteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                    descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    gatt.writeDescriptor(descriptor);
+                    if ((characteristic.getProperties() & BluetoothGattCharacteristic.PROPERTY_READ) != 0) {
+                        gatt.readCharacteristic(characteristic);
+                        Log.d(TAG, "Reading characteristics..");
                     }
 
-                    // to find the ones you're interested in.
-                    // For instance, you can call gatt.getServices() to retrieve a list of available services.
-
-                } else {
-                    Log.w(TAG, "onServicesDiscovered received: " + status);
                 }
+
+                // to find the ones you're interested in.
+                // For instance, you can call gatt.getServices() to retrieve a list of available services.
+
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: " + status);
             }
+        }
 
+    };
 
-            @Override
-            public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-                if (status == BluetoothGatt.GATT_SUCCESS) {
-
-
-                    // TODO: Here you handle the data read from the characteristic.
-                    byte[] data = characteristic.getValue();
-                    Log.d(TAG, "hi" + Arrays.toString(data));
-                    // Call your function with the data
-                }
-            }
-        };
-    }
 }
