f568966334566eb833b94a9c293d96ab137fa01a
greendm1202 <v.shterev@student.utwente.nl>
2023-10-18 12:45:09 +0200

    added swipe right text and fixed position for the data

--- app/src/main/java/com/example/smartgymroom/MainActivity.java
+++ app/src/main/java/com/example/smartgymroom/MainActivity.java
@@ -21,6 +21,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
