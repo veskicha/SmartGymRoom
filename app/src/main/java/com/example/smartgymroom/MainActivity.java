fef2a38c56963e0070c30c9617b9a96ad0ea4561
Iristk <iristenklooster123@gmail.com>
2023-10-26 11:59:59 +0200

    cleaned up more and made it possible to connect to different "rooms"

--- app/src/main/java/com/example/smartgymroom/MainActivity.java
+++ app/src/main/java/com/example/smartgymroom/MainActivity.java
@@ -1,62 +1,105 @@
 package com.example.smartgymroom;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.content.res.ResourcesCompat;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.io.ObjectInputStream;
+
+
+import weka.classifiers.Classifier;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isButtonPressed = false;
+    private MediaManager mediaManager;
+    private static final String TAG = "DebugLogs";
+    private Weka wekaManager;
+    private static final String modelName = "tests/test_model_with_high_sliding_window_size.model";
+    private SensorReading sensors;
+    DataQueueManager manager = new DataQueueManager();
+    TextView predictionTextView;
 
-    @SuppressLint({"MissingPermission"})
+    private Handler handler = new Handler();
+    private Runnable runnableCode = new Runnable() {
+        @Override
+        public void run() {
+            predictionTextView.setText(wekaManager.getActivity());
+            handler.postDelayed(this, 3000);
+        }
+    };
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnableCode); // Stop the loop when the activity is destroyed
+    }
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (checkSelfPermission(Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        }
+        sensors = new SensorReading(manager);
+        wekaManager = new Weka(this, manager);
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensors.initSensors(sensorManager);
+        Log.d("Sensors started", "in oncreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        predictionTextView = findViewById(R.id.prediction_text_view);
+
+        handler.post(runnableCode);
 
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
-            if (item.getItemId() == R.id.currentSession)
-                    selectedFragment = new CurrentSessionFragment();
-            else {
-                    selectedFragment = new HistoryFragment();
+        mediaManager = new MediaManager(this);
+        BluetoothCommunication bluetooth = new BluetoothCommunication(this,"Room 1");
+        bluetooth.startScan();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                if (!isButtonPressed) {
+                    b.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.button_pressed, null));
+                    b.setTextColor(Color.parseColor("#000000"));
+                    b.setText("Stop");
+                    sensors.toggleSensors(sensorManager);
+                } else {
+                    b.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.button_border, null));
+                    b.setTextColor(Color.parseColor("#2fff65"));
+                    b.setText("Start");
+                    sensors.toggleSensors(sensorManager);
+                }
+                isButtonPressed = !isButtonPressed;
             }
-
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-            if (item.getItemId() == R.id.currentSession){
-                fragmentTransaction.setCustomAnimations(
-                        R.anim.slide_in_left,
-                        R.anim.slide_out_right
-                );
-            }else{
-                fragmentTransaction.setCustomAnimations(
-                        R.anim.slide_in_right,
-                        R.anim.slide_out_left
-                );
-            }
-
-            fragmentTransaction.replace(R.id.fragment_container, selectedFragment);
-
-            fragmentTransaction.addToBackStack(null);
-
-            fragmentTransaction.commit();
-            return true;
         });
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new CurrentSessionFragment())
-                .commit();
 
     }
 
+
+    private Classifier initializeWeka() {
+        Classifier classifier = null;
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(getAssets().open(modelName));
+            classifier = (Classifier) objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return classifier;
+    }
 
 }
