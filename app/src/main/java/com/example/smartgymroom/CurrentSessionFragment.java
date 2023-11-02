6312a2ef5f7bd791fee18b209402d8af65e3f16a
greendm1202 <v.shterev@student.utwente.nl>
2023-11-02 14:26:01 +0100

    all merged now

--- app/src/main/java/com/example/smartgymroom/CurrentSessionFragment.java
+++ app/src/main/java/com/example/smartgymroom/CurrentSessionFragment.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+
 import android.graphics.Color;
 import android.hardware.SensorManager;
 import android.os.Bundle;
